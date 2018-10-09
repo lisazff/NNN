11146714e6d9bff2feb32d262e77d19f0df3ba75
lis <zhangfangfang@liwenjindeMacBook-Pro.local>
2018-10-09 15:37:12 +0800

    添加文件

---
+++ NNNDemo/NNNDemo/Business/首页/Model/ZFFollowModel.h
@@ -0,0 +1,13 @@
+//
+//  ZFFollowModel.h
+//  NNNDemo
+//
+//  Created by zhangfangfang on 2018/10/9.
+//  Copyright © 2018年 zhangfangfang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZFFollowModel : NSObject
+
+@end
---
+++ NNNDemo/NNNDemo/Business/首页/Model/ZFFollowModel.m
@@ -0,0 +1,13 @@
+//
+//  ZFFollowModel.m
+//  NNNDemo
+//
+//  Created by zhangfangfang on 2018/10/9.
+//  Copyright © 2018年 zhangfangfang. All rights reserved.
+//
+
+#import "ZFFollowModel.h"
+
+@implementation ZFFollowModel
+
+@end
